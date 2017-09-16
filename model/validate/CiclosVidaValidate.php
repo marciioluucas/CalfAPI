f8a7d8a0e3be64fa5eb0c137076214eb04944cd1
Unknown <brunnoh11@gmail.com>
2017-09-16 16:10:43 -0300

    Ciclos vida

--- model/validate/CiclosVidaValidate.php
+++ model/validate/CiclosVidaValidate.php
@@ -9,7 +9,37 @@
 namespace model\validate;
 
 
-class CiclosVidaValidate
+use Valitron\Validator;
+
+class CiclosVidaValidate implements IValidate
 {
+    public function validatePost($params)
+    {
+        $v = new Validator($params);
+        $v->rule('required', ['enumFaseVida', 'enumLocalizacao', 'fkAnimal']);
+        $v->rule('integer',['fkAnimal']);
+
+        if ($v->validate()) {
+            return true;
+        } else {
+            // Errors
+            return $v->errors();
+        }
+    }
+
+    public function validateGet($params)
+    {
+        // TODO: Implement validateGet() method.
+    }
+
+    public function validatePut($params)
+    {
+        // TODO: Implement validatePut() method.
+    }
+
+    public function validateDelete($params)
+    {
+        // TODO: Implement validateDelete() method.
+    }
 
 }
