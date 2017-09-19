179521324fc8a300ab05ce91e3f281b119096b66
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2017-09-18 22:04:45 -0300

    mudando headers

--- Api.php
+++ Api.php
@@ -16,7 +16,23 @@ class Api
 
     function __construct($url = "")
     {
-        header('Access-Control-Allow-Origin: *');
+        // Allow from any origin
+        if (isset($_SERVER["HTTP_ORIGIN"])) {
+            // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        } else {
+            //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+            header("Access-Control-Allow-Origin: *");
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+            if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+                header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
+            if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            //Just exit with 200 OK with the above headers for OPTIONS method
+            exit(0);
+        }
 
         //--------Responsável pelo REST--------\\
         self::$url = $url;
